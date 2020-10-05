7ab7697b2c3edf9d81cbab4fcb5272510e668af4
Théo <theoposty@gmail.com>
2020-10-05 22:30:01 +0200

    tweak visuel

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="main")
      */
-    public function index()
+    public function main(ArticleRepository $articleRepository)
     {
         return $this->render('main/index.html.twig', [
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/articles", name="articles")
+     */
+    public function articles(CategoryRepository $categoryRepository)
+    {
+        return $this->render('article/articles.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="article")
+     */
+    public function article(Article $article)
+    {
+        return $this->render('article/article.html.twig', [
+            'article' => $article,
         ]);
     }
 }
